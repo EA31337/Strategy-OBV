7b5e1d124642468ce82df09b80afd8f7708d745e
kenorb <kenorb@users.noreply.github.com>
2021-01-11 02:56:38 +0000

    Optimize SignalOpen params (pf=0.00; dd=0.00; tt=0)

--- config/EURUSD_M30.h
+++ config/EURUSD_M30.h
@@ -1,11 +1,16 @@
-/*
+/**
  * @file
  * Defines default strategy parameter values for the given timeframe.
  */
 
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_OBV_Params_M30 : Indi_OBV_Params {
-  Indi_OBV_Params_M30() : Indi_OBV_Params(indi_obv_defaults, PERIOD_M30) { shift = 0; }
+  Indi_OBV_Params_M30() : Indi_OBV_Params(indi_obv_defaults, PERIOD_M30) {
+    applied_price = (ENUM_APPLIED_PRICE)0;
+    ma_method = 0;
+    period = 0;
+    shift = 0;
+  }
 } indi_obv_m30;
 
 // Defines strategy's parameter values for the given pair symbol and timeframe.
@@ -13,14 +18,14 @@ struct Stg_OBV_Params_M30 : StgParams {
   // Struct constructor.
   Stg_OBV_Params_M30() : StgParams(stg_obv_defaults) {
     lot_size = 0;
-    signal_open_method = 0;
-    signal_open_filter = 1;
-    signal_open_level = (float)0;
-    signal_open_boost = 0;
+    signal_open_method = 3;
+    signal_open_filter = 26;
+    signal_open_level = (float)0.0;
+    signal_open_boost = 1;
     signal_close_method = 0;
-    signal_close_level = (float)0;
+    signal_close_level = (float)0.0;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)0.0;
     tick_filter_method = 1;
     max_spread = 0;
   }
