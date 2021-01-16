0139193c8ff95bb5e8ba7c4811806199ba306a7c
kenorb <kenorb@users.noreply.github.com>
2021-01-16 23:07:10 +0000

    Optimize Indicator params (pf=0.49; dd=6.79; tt=5392)

--- config/EURUSD_M5.h
+++ config/EURUSD_M5.h
@@ -6,7 +6,7 @@
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_OBV_Params_M5 : OBVParams {
   Indi_OBV_Params_M5() : OBVParams(indi_obv_defaults, PERIOD_M5) {
-    applied_price = (ENUM_APPLIED_PRICE)0;
+    applied_price = (ENUM_APPLIED_PRICE)5;
     shift = 0;
   }
 } indi_obv_m5;
